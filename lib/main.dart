50762319686d91405d80ab9e68be3dfc74273f74
suojae <suojae.developer@gmail.com>
2025-01-30 14:35:13 +0900

    25.01.30 - blob animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/material.dart';
 
-import '2025/01/29.dart';
+import '2025/01/30.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-final class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.blue,
-        body: Center(child: ShadowBox01()),
+      home: const Scaffold(
+        backgroundColor: Color(0xFFF7EFE5),
+        body: SafeArea(
+          child: Center(
+            child: AnimatedBlob(
+              startColor: Color(0xFFF05A7E),
+              endColor: Color(0xFFFFBE98),
+              gradientDirection: Alignment.topLeft,
+            ),
+          ),
+        ),
       ),
     );
   }
